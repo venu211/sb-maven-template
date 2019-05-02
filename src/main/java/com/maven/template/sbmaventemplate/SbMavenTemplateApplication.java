972fbfc6597a58246da1cd5c16c5ec82d9461fe0
venu <venudondapati2008@gmail.com>
2019-05-02 14:16:01 -0500

    Init repo

---
+++ src/main/java/com/maven/template/sbmaventemplate/SbMavenTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.maven.template.sbmaventemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMavenTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbMavenTemplateApplication.class, args);
+	}
+
+}
